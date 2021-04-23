27e61b2223f72cf5314b1c73372e43467d73c923
TaoSupachai <48078006+TaoSupachai@users.noreply.github.com>
2021-04-23 15:42:10 +0700

    Add files via upload

---
+++ _nuxt/static/1619159348/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619159348",layout:"default",error:a,state:{empData:{},otpresponse:a},serverRendered:true,routePath:"\u002Flogin",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
